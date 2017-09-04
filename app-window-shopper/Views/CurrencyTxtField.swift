e9c9c40cbe5735fece2d7d66b3b907baf11672ac
Fayetan95 <fayetan95@gmail.com>
2017-09-04 10:08:19 +0800

    lesson 01

---
+++ app-window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyTxtField.swift
+//  app-window-shopper
+//
+//  Created by Tan Juat Fwee on 4/9/17.
+//  Copyright © 2017 Faye Tan. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
