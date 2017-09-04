1423496bcf9cd25e5ffaa514304d5090bcb6635a
Fayetan95 <fayetan95@gmail.com>
2017-09-04 15:30:29 +0800

    lesson 04

--- app-window-shopper/Views/CurrencyTxtField.swift
+++ app-window-shopper/Views/CurrencyTxtField.swift
@@ -26,7 +26,7 @@ class CurrencyTxtField: UITextField {
         textAlignment = .center
         
         if let p = placeholder {
-            let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
